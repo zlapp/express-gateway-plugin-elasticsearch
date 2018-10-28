4bef8c9d0c5c482c364765c4886837a105100533
Zvi <zvikush90@gmail.com>
2018-10-28 18:46:38 +0200

    added extract of body

--- manifest.js
+++ manifest.js
@@ -1,16 +1,14 @@
-var elasticsearch = require('elasticsearch');
+var elasticsearch = require('elasticsearch')
 
 var elasticClient = new elasticsearch.Client({
-    host: 'localhost:9200',
-    log: 'trace'
-  });
-  
+  host: 'localhost:9200',
+  log: 'trace'
+})
 
 module.exports = {
   version: '1.0.0',
   policies: ['elasticsearch'],
   init: function (pluginContext) {
-
     pluginContext.registerPolicy({
       schema: {
         $id: 'http://express-gateway.io/policies/metrics.json',
@@ -20,35 +18,36 @@ module.exports = {
             type: 'string',
             default: 'eg-consumer-id'
           }
-        }, required: ['consumerIdHeaderName']
+        },
+        required: ['consumerIdHeaderName']
       },
       name: 'elasticsearch',
       policy: ({ consumerIdHeaderName }) => (req, res, next) => {
         res.once('finish', () => {
-          const apiEndpoint = req.egContext.apiEndpoint.apiEndpointName;
-          const consumerHeader = req.header(consumerIdHeaderName) || 'anonymous';
+          const apiEndpoint = req.egContext.apiEndpoint.apiEndpointName
+          const consumerHeader = req.header(consumerIdHeaderName) || 'anonymous'
           const requestID = req.egContext.requestID
-          const statusCode = res.statusCode.toString();
-          const responseType = res.statusCode >= 200 && res.statusCode < 300 ? 'SUCCESS' : 'FAILED';
-          
+          const statusCode = res.statusCode.toString()
+          const responseType = res.statusCode >= 200 && res.statusCode < 300 ? 'SUCCESS' : 'FAILED'
+
           elasticClient.index({
-                    index: apiEndpoint,
-                    id: requestID,
-                    type: "document",
-                    body: {
-                        title: apiEndpoint,
-                        content: {
-                            consumer:consumerHeader,
-                            code:statusCode,
-                            respType:responseType,
-                            req:req,
-                            res:res
-                        }
-                    }
-                });
-        });
-        next();
+            index: apiEndpoint,
+            id: requestID,
+            type: 'document',
+            body: {
+              title: apiEndpoint,
+              content: {
+                consumer: consumerHeader,
+                code: statusCode,
+                respType: responseType,
+                req: req.body,
+                res: res.body
+              }
+            }
+          })
+        })
+        next()
       }
-    });
+    })
   }
-};
+}
